53f475c75c96678966003129579fba3087f3f815
FHC <fcardoso.poa@gmail.com>
2024-05-16 00:17:04 -0300

    initial commit

---
+++ src/main/java/fhc/loginmoveitback/LoginMoveitBackApplication.java
@@ -0,0 +1,13 @@
+package fhc.loginmoveitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMoveitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginMoveitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhc/loginmoveitback/LoginMoveitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fhc.loginmoveitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMoveitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
