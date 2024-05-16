acb27c1e9a55436170fe53a1fc5d251ea1d6a294
FHC <fcardoso.poa@gmail.com>
2024-05-16 00:48:08 -0300

    domain, repository and tokenService initial implementation

---
+++ src/main/java/fhc/loginmoveitback/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package fhc.loginmoveitback.repositories;
+
+import fhc.loginmoveitback.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
